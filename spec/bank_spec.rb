849fa3be45b993cbebecdf08866f576a88547ca6
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-07 14:30:42 +0100

    added test to check Account class responds to balance method, added balance method, passes tests

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,4 +4,7 @@ describe Account do
   it 'exists' do
     expect(subject).to be_instance_of Account
   end
+  it 'has a readable varible called balance' do
+    expect(subject).to respond_to(:balance)
+  end
 end
